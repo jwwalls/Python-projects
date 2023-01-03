6be9d5a3b3007f3e4c04db6e088cf14f60609f6f
jww <jeremiewwalls@gmail.com>
2023-01-03 00:07:05 -0600

    commit 3

---
+++ cousera_week4.py
@@ -0,0 +1,7 @@
+mask = []
+for x in range(1, 11):
+    mask.append(x*7)
+print(mask) 
+
+mask = [ x* 7 for x in range(1,11)]
+print(mask)
---
+++ cw4.py
@@ -0,0 +1,2 @@
+z = [x for x in range(101) if x % 3 ==0]
+print(z)
